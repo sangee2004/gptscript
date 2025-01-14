5efb28f00ca0f48b9917bf2aaafc51a5a723025b
Sangeetha Hariharan <122560863+sangee2004@users.noreply.github.com>
2025-01-14 14:51:01 -0800

    Update mytest.py
    Signed-off-by: Sangeetha Hariharan <122560863+sangee2004@users.noreply.github.com>

--- tools/testenv/mytest.py
+++ tools/testenv/mytest.py
@@ -3,4 +3,4 @@ import os
 prompt = os.getenv('PROMPT')
 env_var = os.environ['MY_ENV']
 
-print("updated environment"+ ":" + prompt+ ":" +env_var)
+print("updated environment for 1.0 setup"+ ":" + prompt+ ":" +env_var)
